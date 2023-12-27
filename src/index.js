0f54d10bcaef9fe25438d66649eb720e17572f48
nhemmer730 <nhemmer730@gmail.com>
2023-12-27 12:37:40 -0500

    added JS for API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+function displayRecipe(response) {}
+console.log();
 function generateRecipe(event) {
   event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "9faada74e0td93b882032b77odf27ad4";
+  let prompt = "Generate a vegetarian recipe";
+  let context = "";
+  let apiUrl =
+    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
 
   new Typewriter("#recipe", {
     strings: "Generating vegetarian recipe...",
