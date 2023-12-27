57913640a86d21a0f058ba273b942722e16781f8
nhemmer730 <nhemmer730@gmail.com>
2023-12-27 13:31:14 -0500

    Update the index.js with the api and functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-function displayRecipe(response) {}
-console.log();
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9faada74e0td93b882032b77odf27ad4";
-  let prompt = "Generate a vegetarian recipe";
-  let context = "";
-  let apiUrl =
-    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+  let context = `You are a vegetarian food expert and write clear, quick recipes for the user. Start the Instructions on a new line after the ingredients. Make sure to follow the user instructions.`;
+  let prompt = `User instructions: Generate a step-by-step vegetarian recipe about ${instructionsInput.value}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter("#recipe", {
-    strings: "Generating vegetarian recipe...",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
