1dcaffe74ef3e7ac28873281927490272232b191
nhemmer730 <nhemmer730@gmail.com>
2023-12-28 21:30:21 -0500

    Adjusted css and JS context with recipe element

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ function generateRecipe(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9faada74e0td93b882032b77odf27ad4";
-  let context = `You are a vegetarian food expert and write clear, quick recipes for the user. Start the Instructions on a new line after the ingredients. Make sure to follow the user instructions.`;
-  let prompt = `User instructions: Generate a step-by-step vegetarian recipe about ${instructionsInput.value}.`;
+  let context = `You are a vegetarian food expert and write clear, quick recipes for the user. 
+  Please write the recipe as if it was in a cookbook and list out the ingredients. Please write the instructions in a separate list.
+  Make sure to follow the user instructions.`;
+  let prompt = `User instructions: Generate a step-by-step vegetarian recipe with ${instructionsInput.value}.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = `<div class="blink">Generating vegetarian recipe with ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
