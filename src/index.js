fe66ba41810955f506fece5bd2b4d2f399c3186d
nhemmer730 <nhemmer730@gmail.com>
2023-12-29 19:51:37 -0500

    Adjusted language for context for Api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayRecipe(response) {
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 10,
     cursor: "",
   });
 }
@@ -13,8 +13,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9faada74e0td93b882032b77odf27ad4";
   let context = `You are a vegetarian food expert and write clear, quick recipes for the user. 
-  Please write the recipe as if it was in a cookbook and list out the ingredients. Please write the instructions in a separate list.
-  Make sure to follow the user instructions.`;
+  Please write the recipe as if it was in a cookbook.  Make sure to follow the user instructions.`;
   let prompt = `User instructions: Generate a step-by-step vegetarian recipe with ${instructionsInput.value}.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
